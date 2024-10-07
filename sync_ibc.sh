cb5da46512386953b1b7cdc408b2b56715afff63
Reece Williams <31943163+Reecepbcups@users.noreply.github.com>
2024-10-07 13:08:50 -0500

    fix: broken links (#5)
    * cosmos-sdk docs fixes
    
    * wip
    
    * fix ibc /architecture broken links
    
    * fixed: all ibc go relative links
    
    * rm legacy fix
    
    * proto

--- sync_ibc.sh
+++ sync_ibc.sh
@@ -46,12 +46,20 @@ copy_over_core() {
     # docs
     cp -r $DOCS_DIR/docs ./ibc-go
     cp -r $DOCS_DIR/architecture ./ibc-go
+    cp -r $DOCS_DIR/events ./ibc-go
     cp -r $DOCS_DIR/tutorials ./ibc-go
 
     # versioned docs
     cp $DOCS_DIR/versions.json ./ibc-go_versions.json
     cp -r $DOCS_DIR/versioned_sidebars/* ./ibc-go_versioned_sidebars
     cp -r $DOCS_DIR/versioned_docs/* ./ibc-go_versioned_docs
+
+    # find all directories in ./ibc-go_versioned_docs
+    IBC_DIRS=`find ./ibc-go_versioned_docs/ -maxdepth 1 -name "version-*" -type d -exec echo {} \;`
+    for dir in $IBC_DIRS; do
+        cp -r $DOCS_DIR/architecture $dir/
+        cp -r $DOCS_DIR/events $dir/
+    done
 
     # core
     cp $DOCS_DIR/sidebars.js ./ibc-go/sidebars.js
@@ -67,6 +75,18 @@ copy_over_core() {
 fix_references() {
     OLD="../../../../docs/"; NEW="../../../../ibc-go/docs/"
     replace "./ibc-go_versioned_docs" "$OLD" "$NEW"
+
+    FILES=`find ./ibc-go_versioned_docs -type f -name "*.md"`
+    for file in $FILES; do
+        sed -i "s#(/architecture/#(/ibc-go/architecture/#g" $file
+        sed -i "s#(/events/events#(/ibc-go/events/#g" $file
+    done
+
+    FILES=`find ./ibc-go -type f -name "*.md"`
+    for file in $FILES; do
+        sed -i "s#(/architecture/#(/ibc-go/architecture/#g" $file
+        sed -i "s#(/events/events#(/ibc-go/events/#g" $file
+    done
 }
 
 fix_components() {
